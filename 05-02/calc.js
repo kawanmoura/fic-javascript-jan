1faf5d22ea7cedfb23b0c79036e27ec193cda659
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-05 15:44:53 -0300

    Script

---
+++ 05-02/calc.js
@@ -0,0 +1,11 @@
+// Duas variáveis
+// Num1 e Num2
+// Pergunta para o usuário
+let num1 = Number(prompt("Digite um número:"));
+let num2 = Number(prompt("Digite outro número:"));
+
+console.log(typeof num1);
+console.log(typeof num1);
+
+// Imprimir resposta
+console.log(`A soma é ${num1 + num2}`);
