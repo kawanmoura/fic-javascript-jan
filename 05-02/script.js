28563f997c27095e4c8854897f955908d3fdac9e
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-05 15:24:43 -0300

    JavaScript

--- 05-02/script.js
+++ 05-02/script.js
@@ -1,9 +1,10 @@
 // Como se fosse um "span" (Var)
 let nome = prompt("Qual seu nome?");
 let idade = prompt("Quantos Anos?");
-let time = prompt("Que time é teu?");
+let time = prompt("Fiel de qual time?");
 
 // Imprimir na tela
-document.write("Seja Bem-Vindo: " + nome + ", Sua Idade é: " + idade + " Anos");
 document.write("<br><br>");
 document.write(`Seja Bem-Vindo: ${nome}, Sua Idade é: ${idade} anos.`);
+document.write("<br>");
+document.write(`Seu time é: ${time}.`);
