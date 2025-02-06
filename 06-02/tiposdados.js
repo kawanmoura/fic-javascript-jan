f833237395a89ae808ee809d2a44857476cf7f93
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-06 14:41:27 -0300

    adicionando atividades

---
+++ 06-02/tiposdados.js
@@ -0,0 +1,16 @@
+// String (Variáveis de texto)
+let nome = "Kawan";
+
+// Int (Números inteiros)
+let idade = 17;
+
+// Boolean (Verdadeiro ou falso)
+let casado = true;
+
+// Booleanos (Números flutuantes com casas decimais)
+let valorProduto = 5.35;
+
+console.log("Meu nome é: " + nome);
+console.log("Idade: " + idade);
+console.log("Casado? " + casado);
+console.log("R$ " + valorProduto);
---
+++ 06-02/variável.js
@@ -0,0 +1,12 @@
+let mensagem = "Hello World";
+const nomeCompleto = "Kawan Moura";
+
+// nomeCompleto =  "Kawan Moura";
+
+// Idade
+let idade = 17;
+
+idade = 18;
+
+console.log(nomeCompleto);
+console.log(`${mensagem} e a idade é ${idade}`);
