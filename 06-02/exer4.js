81403956d5d85a0c6b5590090bfd119364407c31
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-06 15:47:47 -0300

    exer 4 e 5

---
+++ 06-02/exer4.js
@@ -0,0 +1,4 @@
+const mozilla = "Mozilla Deloper Network";
+const MDN = mozilla[0] + mozilla[8] + mozilla[16];
+
+console.log(MDN);
---
+++ 06-02/exer5.js
@@ -0,0 +1,10 @@
+/*
+    - Exiba no console do browser a 
+    quantidade de caracteres que a const possui.
+*/
+
+const linguagem = "JavaScript";
+
+linguagem.length = linguagem;
+console.log(linguagem);
+console.log(linguagem.length);
