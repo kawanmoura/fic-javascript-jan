63a873cf907f2205271094cb333352f204d845fe
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-07 16:20:13 -0300

    exer3

---
+++ 07-02/exer-if-3.js
@@ -0,0 +1,11 @@
+/*
+    se idade >= 18, a pessoa pode ir pra festa
+    se não, a pessoa não pode ir pra festa
+*/
+let idade = 18;
+
+if (idade <= 16) {
+    console.log("Pode vir à festa");
+} else {
+    console.log("Rala daqui");
+}
