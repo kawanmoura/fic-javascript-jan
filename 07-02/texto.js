db4aef43ba2335a9ae33bc802f3b6caa9f8b601b
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-07 14:27:36 -0300

    texto

--- 07-02/texto.js
+++ 07-02/texto.js
@@ -1,6 +1,12 @@
 let text = "Estão chegando as provas";
 
+// tamanho da string
 console.log(text.length);
 
-console.log(text.toLowerCase);
-console.log(text.toUpperCase);
+// replace
+console.log("Alterando", text.replace("provas", 'Avaliações'));
+console.log(text);
+
+
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
