21a03d4419810b351d3aeaf72a89716f9416e809
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-07 15:45:31 -0300

    exer2

---
+++ 07-02/exer2.js
@@ -0,0 +1,6 @@
+const maleDogNames = ["Chico", "Zeca"];
+const femaleDogNames = ["Doris", "Juliana"];
+const dogs = maleDogNames.concat(femaleDogNames);
+
+console.log(dogs)
+// o resultado vai ser: ["a", "b", "c", "d", "e", "f"]
