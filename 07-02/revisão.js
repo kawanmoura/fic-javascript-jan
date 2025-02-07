ace01eba5800b7839e6a6c631ad1221f8f746f27
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-07 14:14:01 -0300

    dia 7/2

---
+++ 07-02/revisão.js
@@ -0,0 +1,9 @@
+let mensagem = "Olá Kawan";
+let idade = 18;
+let casado = false;
+let times = ["Flamengo", "Corinthians", 12, true];
+
+console.log(mensagem);
+console.log(idade);
+console.log(casado);
+console.log(times[1]);
---
+++ 07-02/texto.js
@@ -0,0 +1,6 @@
+let text = "Estão chegando as provas";
+
+console.log(text.length);
+
+console.log(text.toLowerCase);
+console.log(text.toUpperCase);
