4239690d4be4e69f7b0a24b740d31f76e10c4ab8
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-07 14:36:39 -0300

    texto2

---
+++ 07-02/texto2.js
@@ -0,0 +1,6 @@
+let text = "Estão chegando as provas";
+
+// retorna a primeira posição na string
+console.log(text.indexOf("as"));
+// retorna a última posição na string
+console.log(text.lastIndexOf("as"));
