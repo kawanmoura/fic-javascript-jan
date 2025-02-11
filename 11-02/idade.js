d2d4520e577bafb23a88f1a7397c8f6fdfe37c4d
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-11 14:02:05 -0300

    if de idades

---
+++ 11-02/idade.js
@@ -0,0 +1,11 @@
+let idade = 88;
+
+if (idade <= 12) {
+  console.log('É uma criança!');
+} else if (idade <= 17) {
+  console.log('É um adolescente!');
+}else if (idade <= 29) {
+  console.log('Jovem adulto!');
+}else {
+  console.log('Adulto!');
+}
