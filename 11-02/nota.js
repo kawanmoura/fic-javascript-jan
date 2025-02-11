ef5274abb82a217e29ff00eb07a68767ed223dc6
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-11 14:36:33 -0300

    notas

---
+++ 11-02/nota.js
@@ -0,0 +1,12 @@
+/*
+    se a nota do aluno for maior ou igual a 6, ele está aprovado
+    se não, ele está reprovado
+*/
+
+const nota = 5;
+
+if (nota >= 6) {
+    console.log('Aprovado');
+} else {
+    console.log('Reprovado');
+}
