1d9d6adef6370efec57ab9bc57b7e21723ece912
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-12 15:31:17 -0300

    nomes com -

---
+++ 12-02/nomes.js
@@ -0,0 +1,10 @@
+/*
+    fazer com que cada nome do array fique um um "-" antes, pulando linha 
+    para cada um.
+*/
+
+const nomes = ["Fernando", "Caio", "Juliana"];
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log("-" + nomes[i]);
+}
