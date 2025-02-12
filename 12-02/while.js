ba0f73a0e5d038e624807a95eb78b9bd746311d2
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-12 16:12:43 -0300

    especificação da atividade while

--- 12-02/while.js
+++ 12-02/while.js
@@ -1,1 +1,12 @@
 // while
+/*
+  - Utilizando um while loop, exiba no console todos os números entre 0 e 5,  
+    inclusive esses dois.
+  - O resultado deve ser:
+    0
+    1
+    2
+    3
+    4
+    5
+*/
