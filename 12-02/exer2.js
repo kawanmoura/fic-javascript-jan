cd72a7954c95541945b2311e97bc11e086ae9d0b
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-12 14:54:52 -0300

    cidades em for loop

---
+++ 12-02/exer2.js
@@ -0,0 +1,19 @@
+/*
+    -Abaixo da constante "cities", declare uma let "upperCaseCities" e atribua à
+    ela um array vazio;
+    -Adicione ao array que a let "upperCaseCities" armazena as cidades do array
+    cities com todas as letras maiúsculas, utilizando o for loop;
+    -Para ver se deu certo, exiba o upperCaseCities no console.
+    push() - dica
+    [] array vazio - dica
+*/
+
+const cities = ["São Paulo", "Rio de Janeiro", "Minas Gerais"];
+let upperCaseCities = [];
+console.log(cities.length);
+
+for (let i = 0; i < cities.length; i++) {
+    upperCaseCities.push(cities[i].toUpperCase());
+}
+
+console.log(upperCaseCities);
