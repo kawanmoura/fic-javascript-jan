d4d6f06994bf5fec6644b084fa06f76ac67ce275
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-13 14:39:26 -0300

    outra tabuada do 7

--- 13-02/while-exer.js
+++ 13-02/while-exer.js
@@ -7,3 +7,16 @@ while (multiplicador <= 10) {
     document.write(`7 * ${multiplicador} = ${7 * multiplicador} <br>`);
     multiplicador++;
 }
+
+
+
+//outra maneira
+function tabuada7() {
+    let multiplicador = 1;
+
+    while (multiplicador <= 10) {
+        document.write(`7 * ${multiplicador} = ${7 * multiplicador} <br>`);
+        multiplicador++;
+    }
+}
+tabuada7();
