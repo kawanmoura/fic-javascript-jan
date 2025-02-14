fb03414d5352657155c148046a73e26cbfff611f
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-14 16:05:45 -0300

    if e else

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -3,6 +3,14 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
 
 function resultadoNotaClick(event) {
+    // Previne um comportamento no navegador
     event.preventDefault();
     console.log(inputNota.value);
-}
+
+    // Condição
+    if (inputNota.value >= 6) {
+        console.log("Aprovado");
+    } else {
+        console.log("Reprovado");
+    }
+} 
