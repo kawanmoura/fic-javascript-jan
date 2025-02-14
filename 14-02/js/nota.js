77eeeb0d28712f0993bcbc889983db7297163d33
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-14 15:48:00 -0300

    js

---
+++ 14-02/js/nota.js
@@ -0,0 +1,8 @@
+// Variáveis
+const nota = document.getElementById("nota");
+const resultado = document.getElementById("resultado");
+
+function resultadoNotaClick() {
+    console.log("Olá, Kawan!");
+    alert("Olá, Kawan!");
+}
