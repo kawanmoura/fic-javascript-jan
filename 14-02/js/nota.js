f2fe4ac5ee0027765f1e6fc4ce3f1812eec6e9f7
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-18 14:15:27 -0300

    atualizando

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -1,11 +1,18 @@
 // Variáveis
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
+const message = document.getElementById("message");
 
 function resultadoNotaClick(event) {
     // Previne um comportamento no navegador
     event.preventDefault();
     console.log(inputNota.value);
+
+    // Validação de formulário
+    if (inputNota.value == "") {
+        // alert("Digite a nota.");
+        return false;
+    }
 
     // Condição
     if (inputNota.value >= 6) {
