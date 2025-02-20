8c66a419d84bdaafd6fc1a025e75c317d19530ac
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-20 14:18:03 -0300

    atualizando

---
+++ 20-02/JS/cadastro.js
@@ -0,0 +1,8 @@
+// Variáveis
+const campo = document.querySelector("#campo");
+const salvar = document.querySelector("#salvar");
+
+// Adicionar evento de clique ao botão salvar
+salvarBtn.addEventListener("click", function() {
+    alert("teste")
+});
