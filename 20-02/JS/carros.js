701aa55cfa63827560f1698b45ebd7b5ca1ad8f1
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-21 14:44:26 -0300

    adicionando carros

---
+++ 20-02/JS/carros.js
@@ -0,0 +1,18 @@
+const cars = [
+    {
+        brand:"Toyota",
+        model:"Supra",
+        year: 1993
+    },
+    {
+        brand:"Honda",
+        model:"NSX",
+        year: 1991
+    },
+];
+
+console.log(cars[0].model);
+// Ano do honda
+console.log(cars[1].year);
+// Ano do toyota
+console.log(cars[0].year);
