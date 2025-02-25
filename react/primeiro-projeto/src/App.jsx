fecb849f7413713454763614e78d86d790edbfec
kawanmoura <kawanlemesmoura@gmail.com>
2025-02-25 16:08:49 -0300

    site kawan

--- react/primeiro-projeto/src/App.jsx
+++ react/primeiro-projeto/src/App.jsx
@@ -1,10 +1,19 @@
+import Header from "./components/Header";
+
 function App() {
 
   return (
     <>
+      <div className="container">
+        <Header />
+
+        <h1>Seja Bem-Vindo</h1>
+
         <p>
-          Kawan
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis labore voluptatibus fugit ab doloremque quidem laudantium officia 
+          iure, eveniet animi voluptate repudiandae repellat sapiente similique alias nulla commodi a harum? Commodi placeat velit voluptatem architecto impedit hic id ullam voluptate? Cumque aspernatur nobis doloribus veniam adipisci exercitationem, dolorum ad perferendis.
         </p>
+      </div>
     </>
   )
 }
